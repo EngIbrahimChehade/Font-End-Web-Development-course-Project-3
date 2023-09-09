25f4cc9a34f29e09c4bb938dfdd7c7cd7721262c
EngIbrahimChehade <Eng.IbrahimChehade@hotmail.com>
2023-09-09 16:03:17 +0300

    First check in

---
+++ website/app.js
@@ -0,0 +1,53 @@
+/* Global Variables */
+let baseURL = "";
+const apiKey = "f5a72853985a1b0c3d1d398a15ab2605&units=imperial";
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// Async POST
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      return newData;
+    }catch(error) {
+    console.log("error", error);
+    }
+};
+
+// Async GET
+const retrieveData = async (url='') =>{ 
+  const request = await fetch(url);
+  try {
+  // Transform into JSON
+  const allData = await request.json()
+  }
+  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+
+newWeather = {
+    temperature : "12",
+    date : "13",
+    userResponse : "14"
+}
+
+postData('/addWeather', newWeather);
+// TODO-Chain your async functions to post an animal then GET the resulting data
+
+// TODO-Call the chained function
